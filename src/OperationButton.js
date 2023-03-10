9fab7f5495db5120320a2f865b1bdd43d61da087
tomasbrezina <tomas.brezinaaa@gmail.com>
2023-03-10 19:07:49 +0100

    operation button

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,5 +1,5 @@
 import { ACTIONS } from "./App"
 
 export default function DigitButton({ dispatch, operation }) {
-    return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>{operation}</button>
+    return <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}>{operation}</button>
 }
